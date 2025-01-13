60f8c222641ed01bee7b2ad3b36ad4fb60f38148
Mariano Montero <gibran.colio@gmail.com>
2025-01-13 15:42:59 -0600

    Fix linting

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -23,28 +23,25 @@ class AdminSiteTest(TestCase):
       name='Test user'
     )
 
-
   def test_user_list(self):
     """Test that user are listed on page"""
     url = reverse('admin:core_user_changelist')
     # Makes and get HTTP request
     res = self.client.get(url)
-    
+
     self.assertContains(res, self.user.name)
     self.assertContains(res, self.user.email)
-
 
   def test_edit_user_page(self):
     """Test the edit user page works"""
     url = reverse('admin:core_user_change', args=[self.user.id])
     res = self.client.get(url)
-    
-    self.assertEqual(res.status_code, 200)
 
+    self.assertEqual(res.status_code, 200)
 
   def test_create_user_page(self):
     """test the create user page works"""
     url = reverse('admin:core_user_add')
     res = self.client.get(url)
-    
+
     self.assertEqual(res.status_code, 200)
