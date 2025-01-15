c3f33b81c8376897626db921f74ddd22796747d9
Mariano Montero <mariano@hmdtrucking.com>
2025-01-15 16:52:50 -0600

    Added tag model, created test and logic to retrieve tag's list

--- app/core/models.py
+++ app/core/models.py
@@ -17,7 +17,7 @@ class UserManager(BaseUserManager):
     user = self.model(email=self.normalize_email(email), **extra_fields)
     user.set_password(password)
     user.save(using=self._db)
-    
+
     return user
 
 
@@ -27,7 +27,7 @@ class UserManager(BaseUserManager):
     user.is_staff = True
     user.is_superuser = True
     user.save(using=self._db)
-    
+
     return user
 
 class User(AbstractBaseUser, PermissionsMixin):
@@ -36,9 +36,9 @@ class User(AbstractBaseUser, PermissionsMixin):
   name = models.CharField(max_length=255)
   is_active = models.BooleanField(default=True)
   is_staff = models.BooleanField(default=False)
-  
+
   objects = UserManager()
-  
+
   USERNAME_FIELD = 'email'
 
 class Recipe(models.Model):
@@ -52,6 +52,18 @@ class Recipe(models.Model):
   time_minutes = models.IntegerField()
   price = models.DecimalField(max_digits=5, decimal_places=2)
   link = models.CharField(max_length=255, blank=True)
-  
+  tags = models.ManyToManyField('Tag')
+
   def __str__(self):
     return self.title
+
+class Tag(models.Model):
+  """Tag for filtering recipes."""
+  name = models.CharField(max_length=255)
+  user = models.ForeignKey(
+    settings.AUTH_USER_MODEL,
+    on_delete=models.CASCADE,
+  )
+
+  def __str__(self):
+    return self.name
